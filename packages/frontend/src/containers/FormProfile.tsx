f09a857c56431cc51b0a6a0ac987341682dfbec4
Andri <andri@bizoftenterprise.com>
2022-06-03 15:35:08 +0700

    Add edit profile form

--- packages/frontend/src/containers/FormProfile.tsx
+++ packages/frontend/src/containers/FormProfile.tsx
@@ -1,49 +1,24 @@
-import { Field, FieldProps, Formik } from "formik";
+import { FieldProps, Formik } from "formik";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Grid from "@mui/material/Grid";
 import Switch from "@mui/material/Switch";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import * as yup from "yup";
-import { useEffect, useState } from "react";
-import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
-import Select from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
-import { ApolloError } from "@apollo/client";
+import { useState } from "react";
 import { useSnackbar } from "../components/SnackBarProvider";
 import { useWalletProvider } from "../contexts/wallet.provider";
 import { useUpdateUserProfileMutation } from "../generated/graphql";
+import { usePageInfoProvider } from "./Layout";
 
 const validationSchema = yup.object().shape({
   name: yup.string().min(2, "Too Short!").max(100, "Too Long!").nullable(),
   email: yup.string().email("Email invalid").nullable(),
 });
 
-interface Props {
-  // onSubmit: (value: NftData) => void;
-}
-
-const FormikSwitch: React.ComponentType<FieldProps> = ({ field, form }) => {
-  return (
-    <FormControlLabel
-      control={
-        <Switch
-          defaultChecked={field.value}
-          value={field.value}
-          onChange={(e) => {
-            form.setFieldValue(field.name, e.target.checked);
-          }}
-        />
-      }
-      label="Free Minting"
-    />
-  );
-};
-
-const FormProfile: React.FC<Props> = () => {
+const FormProfile: React.FC = () => {
   const snackbar = useSnackbar();
-  const [loading, setLoading] = useState(false);
+  const { setLoading } = usePageInfoProvider();
 
   const { user, provider, updateUser } = useWalletProvider();
 
@@ -56,23 +31,17 @@ const FormProfile: React.FC<Props> = () => {
     onCompleted: (data) => {
       if (!data.UpdateUserProfile) return;
       updateUser(data.UpdateUserProfile);
+      setLoading(false);
     },
   });
 
   return (
     <>
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={loading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-
       <Formik
         initialValues={user!}
         validationSchema={validationSchema}
-        // onSubmit={onSubmit}
         onSubmit={(data) => {
+          setLoading(true);
           updateGQLUser({
             variables: {
               id: user!.id,
